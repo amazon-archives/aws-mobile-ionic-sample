27b34b4fd6531c6d75de040612c66f25e0e12bdc
Hayato Yamauchi <yamauchi@r3it.com>
2018-04-17 22:16:35 +0900

    Currently, since 'AWS Mobile Hub Management Console' exports 'aws_cloud_logic_custom' as an array, JSON parse is unnecessary.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -9,14 +9,13 @@ declare const aws_cloud_logic_custom
 @Injectable()
 export class AwsConfig {
   public load () {
-    let aws_cloud_logic_custom_obj = JSON.parse(aws_cloud_logic_custom)
     return {
       'region': aws_cognito_region, // region you are deploying (all lower caps, e.g: us-east-1)
       'userPoolId': aws_user_pools_id, // your user pool ID
       'appId': aws_user_pools_web_client_id, // your user pool app ID
       'idpURL': `cognito-idp.${aws_cognito_region}.amazonaws.com`, // cognito idp url
       'identityPool': aws_cognito_identity_pool_id, // your federated identity pool ID
-      'APIs': aws_cloud_logic_custom_obj.reduce((m, v) => { m[v.name] = v.endpoint; return m }, {})
+      'APIs': aws_cloud_logic_custom.reduce((m, v) => { m[v.name] = v.endpoint; return m }, {})
     }
   }
 }
